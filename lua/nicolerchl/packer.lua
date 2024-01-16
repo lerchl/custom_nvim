c1a51baff552d9d5deb76bc81093f551ef277293
Nico Lerchl <nico@lerchl.one>
2024-01-16 11:57:25 +0100

    init

---
+++ lua/nicolerchl/packer.lua
@@ -0,0 +1,61 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+    'embark-theme/vim',
+    as = 'embark',
+    config = function()
+      vim.cmd('colorscheme embark')
+    end
+  }
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+
+  use ('github/copilot.vim')
+
+  use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lua'},
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'}
+    }
+  }
+end)
+
---
+++ lua/nicolerchl/remap.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>")
+vim.keymap.set("n", "<C-s>", ":w<CR>")
+
