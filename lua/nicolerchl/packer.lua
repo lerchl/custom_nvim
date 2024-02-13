0279dce4671167a776072f7c2eff6cc018bdf3b5
Nico Lerchl <nico.lerchl-cic@ibm.com>
2024-02-13 13:52:17 +0100

    neogit and shift tab to close lsp suggestions

--- lua/nicolerchl/packer.lua
+++ lua/nicolerchl/packer.lua
@@ -4,103 +4,108 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  use {
-    'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    end
-  }
+	use {
+		'goolord/alpha-nvim',
+		config = function ()
+			require'alpha'.setup(require'alpha.themes.dashboard'.config)
+		end
+	}
 
+	use {
+		'folke/tokyonight.nvim',
+		config = function()
+			vim.cmd('colorscheme tokyonight-night')
+		end
+	}
 
-  use {
-        'folke/tokyonight.nvim',
-        config = function()
-            vim.cmd('colorscheme tokyonight-night')
-        end
-  }
+	use {
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end
+	}
 
-  -- use {
-  --       'nyoom-engineering/oxocarbon.nvim',
-  --       as = 'oxocarbon',
-  --       config = function()
-  --           vim.cmd('colorscheme oxocarbon')
-  --       end
-  -- }
+	use {
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",         -- required
+			"sindrets/diffview.nvim",        -- optional - Diff integration
 
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = function()
+			require('neogit').setup()
+		end
+	}
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+	}
 
-  use "f-person/git-blame.nvim"
+	use "f-person/git-blame.nvim"
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-  use "lukas-reineke/indent-blankline.nvim"
-  use "echasnovski/mini.indentscope"
+	use "lukas-reineke/indent-blankline.nvim"
+	use "echasnovski/mini.indentscope"
 
-  use "terrortylor/nvim-comment"
+	use "terrortylor/nvim-comment"
 
-  use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+	use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
 
-  use ('github/copilot.vim')
+	use ('github/copilot.vim')
 
-  use {'stevearc/dressing.nvim'}
+	use {'stevearc/dressing.nvim'}
 
-  use { "akinsho/toggleterm.nvim", tag = '*' }
+	use { "akinsho/toggleterm.nvim", tag = '*' }
 
-  use {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  }
+	use {
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		}
+	}
 
-  use "echasnovski/mini.pairs"
+	use "echasnovski/mini.pairs"
 
-  use { "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" }
+	use { "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" }
 
-  use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+	use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    requires = {
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lua'},
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'}
-    }
-  }
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lua'},
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'}
+		}
+	}
 end)
 
