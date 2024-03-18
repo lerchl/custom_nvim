9142da124fd74be36eb92ba3892509dfbefa08f8
Nico Lerchl <nico.lerchl-cic@ibm.com>
2024-03-18 14:22:30 +0100

    colors

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,3 +1,5 @@
+vim.cmd("colorscheme tokyonight-moon")
+
 require("tokyonight").setup {
 	style = "moon",
 	terminal_colors = true,
@@ -5,6 +7,19 @@ require("tokyonight").setup {
 		comments = { italic = false },
 		keywords = { italic = false },
 	},
+	on_highlights = function(hl, c)
+		hl.BufferlineFileIcon = {
+			bg = c.bg_highlight
+		}
+
+		hl.BufferlineFill = {
+			bg = c.bg_statusline
+		}
+
+		hl.NeoTreeNormal = {
+			bg = c.bg
+		}
+	end
 }
 
 vim.cmd("colorscheme tokyonight-moon")
