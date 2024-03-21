73e3a9671a44f18bb6da540ed81ec21379fcae30
Nico Lerchl <nico.lerchl-cic@ibm.com>
2024-03-21 11:53:50 +0100

    use libuv file watcher

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,26 +1,28 @@
 require("neo-tree").setup {
 	enable_git_status = false,
-    filesystem = {
-        group_empty_dirs = true
-    },
+	filesystem = {
+		group_empty_dirs = true,
+		use_libuv_file_watcher = true
+	},
 	window = {
 		position = "current"
 	}
 }
 
-vim.keymap.set('n', '-', function()
-    local reveal_file = vim.fn.expand('%:p')
-    if (reveal_file == '') then
-        reveal_file = vim.fn.getcwd()
-    else
-        local f = io.open(reveal_file, "r")
-        if (f) then
-            f.close(f)
-        else
-            reveal_file = vim.fn.getcwd()
-        end
-    end
-    require('neo-tree.command').execute({ reveal_file = reveal_file }) end,
-    { desc = "Show current file in neotree" }
-);
+local show_current_file = function()
+	local reveal_file = vim.fn.expand('%:p')
+	if (reveal_file == '') then
+		reveal_file = vim.fn.getcwd()
+	else
+		local f = io.open(reveal_file, "r")
+		if (f) then
+			f.close(f)
+		else
+			reveal_file = vim.fn.getcwd()
+		end
+	end
+	require('neo-tree.command').execute({ reveal_file = reveal_file })
+end
+
+vim.keymap.set('n', '-', show_current_file, { desc = "Show current file in neotree" });
 
