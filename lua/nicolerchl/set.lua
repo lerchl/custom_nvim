ee8da92b99d8749262c737e35dea8697a70b4d18
Nico Lerchl <nico.lerchl-cic@ibm.com>
2024-04-04 11:55:31 +0200

    changes

--- lua/nicolerchl/set.lua
+++ lua/nicolerchl/set.lua
@@ -7,17 +7,11 @@ vim.opt.expandtab = false
 vim.opt.smartindent = true
 
 vim.opt.incsearch = true
-
 vim.opt.termguicolors = true
-
 vim.opt.cursorline = true
 vim.opt.scrolloff = 12
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
 vim.opt.updatetime = 50
-
--- vim.opt.colorcolumn = "80,120"
-
 vim.opt.hidden = true
-
+vim.opt.fixendofline = true
