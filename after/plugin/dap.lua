9de0981331046b8d7274b8181bd618265d210123
Nico Lerchl <nico@lerchl.one>
2024-04-08 18:21:00 +0200

    debug keymaps

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,4 +1,11 @@
-require("dapui").setup()
+local dap = require("dap")
+vim.keymap.set("n", "<F5>", dap.continue, { desc = "Continue" })
+vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Continue" })
+vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Continue" })
+vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
 
-vim.keymap.set("n", "<leader>dd", ":lua require('dapui').toggle()<CR>", { desc = "Toggle DAP UI" })
+local dapui = require("dapui")
+dapui.setup()
+
+vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle DAP UI" })
 
