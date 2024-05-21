17e493bfdfccac4bd3447380ddbff1119c49a61a
Nico Lerchl <nico.lerchl-cic@ibm.com>
2024-05-21 10:12:18 +0200

    remove precognition

--- after/plugin/precognition.lua
+++
@@ -1,2 +0,0 @@
-require("precognition").toggle()
-
--- lua/nicolerchl/packer.lua
+++ lua/nicolerchl/packer.lua
@@ -40,8 +40,6 @@ return require('packer').startup(function(use)
 
 	use "f-person/git-blame.nvim"
 
-	use "tris203/precognition.nvim"
-
 	use {
 		'nvim-telescope/telescope.nvim',
 		requires = {{ 'nvim-lua/plenary.nvim' }}
