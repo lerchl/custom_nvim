76f95b5219486e1a946918f37069ed2b5b80aa60
Nico Lerchl <nico.lerchl-cic@ibm.com>
2024-10-10 13:55:41 +0200

    keymaps

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,1 +1,5 @@
 require("trouble").setup()
+
+vim.keymap.set("n", "<leader>ts", "<cmd>Trouble symbols toggle win.relative=win win.position=right<cr>", { desc = "Toggle Symbols" })
+vim.keymap.set("n", "<leader>td", "<cmd>Trouble diagnostics toggle filter.buf=0 win.relative=win win.position=bottom<cr>", { desc = "Toggle Diagnostics" })
+
