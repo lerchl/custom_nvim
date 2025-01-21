140baea3888924d4f304ec5f36804bd429e4b9ab
Nico Lerchl <nico.lerchl-cic@ibm.com>
2025-01-21 15:26:55 +0100

    theme

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -2,9 +2,14 @@
 local main = "#867bc9"
 local base = "#070707"
 
+local darken = require("catppuccin.utils.colors").darken
+
 require("catppuccin").setup {
 	flavour = "mocha",
 	no_italic = true,
+	integrations = {
+    	diffview = true
+	},
 	color_overrides = {
 		mocha = {
 			base = base,
@@ -17,15 +22,15 @@ require("catppuccin").setup {
 			WinSeparator = { fg = main },
 			FloatBorder = { fg = main },
 			VertSplit = { fg = main },
-			NeoTreeTitleBar = { fg = "#EEEEEE", bg = main },
+			NeoTreeTitleBar = { fg = "#FFFFFF", bg = main },
 			NeoTreeRootName = { fg = main },
 			NeoTreeDirectoryName = { fg = main },
 			NeoTreeDirectoryIcon = { fg = main },
 			LineNr = { fg = "#888888" },
-			DiffAdd = { fg = C.green },
-			DiffChange = { fg = C.gray },
-			DiffDelete = { fg = C.red },
-			DiffText = { fg = C.blue }
+			DiffAdd = { bg = darken(C.green, 0.18, C.base) }, -- diff mode: Added line |diff.txt|
+			DiffChange = { bg = darken(C.blue, 0.07, C.base) }, -- diff mode: Changed line |diff.txt|
+			DiffDelete = { bg = darken(C.red, 0.18, C.base) }, -- diff mode: Deleted line |diff.txt|
+			DiffText = { bg = darken(C.blue, 0.30, C.base) }, -- diff mode: Changed text within a changed line |diff.txt|	
 		}
 	end
 }
