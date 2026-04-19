041c5dd796411d3da1e566ebc2b5086db315a509
Nico Lerchl <nico@lerchl.one>
2026-04-19 14:43:12 +0200

    feat: config for some lsps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,70 +1,80 @@
 local on_attach = require("nicolerchl.lsp").on_attach
 
 require("mason").setup()
-require("mason-lspconfig").setup {
-    ensure_installed = {
-        "lua_ls",
-        "clangd",
-        "jdtls",
-        "rust_analyzer",
-        "ts_ls",
-        "eslint",
-        "helm_ls",
-        "yamlls",
-    },
-    automatic_enable = {
-        exclude = {
-            "jdtls",
-        }
-    }
-}
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"clangd",
+		"jdtls",
+		"rust_analyzer",
+		"ts_ls",
+		"eslint",
+		"helm_ls",
+		"yamlls",
+	},
+	automatic_enable = {
+		exclude = {
+			"jdtls",
+		},
+	},
+})
 
 local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 vim.lsp.config("lua_ls", {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = { globals = { "vim" } },
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-        },
-    },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+		},
+	},
 })
 
 vim.lsp.config("eslint", {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        rulesCustomizations = { { rule = "*", severity = "warn" } },
-    },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		rulesCustomizations = { { rule = "*", severity = "warn" } },
+	},
 })
 
 vim.lsp.config("ts_ls", {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        typescript = { includeInlayParameterNameHints = "literals" },
-    },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		typescript = { includeInlayParameterNameHints = "literals" },
+	},
+})
+
+vim.lsp.config("yamlls", {
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 vim.lsp.config("helm_ls", {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        ["helm-ls"] = {
-            yamlls = { path = vim.fn.exepath("yaml-language-server") },
-        },
-    },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		["helm-ls"] = {
+			yamlls = { path = vim.fn.exepath("yaml-language-server") },
+		},
+	},
 })
 
 vim.lsp.config("angularls", {
-    capabilities = capabilities,
-    on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 vim.lsp.config("rust_analyzer", {
-    capabilities = capabilities,
-    on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+
+vim.lsp.config("clangd", {
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
