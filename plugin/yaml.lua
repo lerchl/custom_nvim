3457496bacc5ac60a95dfcbef3c813abc19f9805
Nico Lerchl <nico.lerchl-cic@ibm.com>
2026-07-10 14:45:44 +0200

    feat: better yaml tekton detection

--- plugin/yaml.lua
+++ plugin/yaml.lua
@@ -1,10 +1,21 @@
 local util = require("lspconfig.util")
 
+local function is_tekton_file(filepath)
+    local f = io.open(filepath, "r")
+    if not f then return false end
+    local content = f:read("*a")
+    f:close()
+    return content:match("apiVersion:%s*tekton%.dev/") ~= nil
+end
+
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
     pattern = { "*.yaml", "*.yml" },
     callback = function()
-        local root = util.root_pattern("Chart.yaml")(vim.fn.expand("%:p"))
-
+        local filepath = vim.fn.expand("%:p")
+        if is_tekton_file(filepath) then
+            return
+        end
+        local root = util.root_pattern("Chart.yaml")(filepath)
         if root then
             vim.bo.filetype = "helm"
         end
